861bbd177ae0bc7fad8a13b1cea80614b5f39f61
John <johnplant1215@gmail.com>
2019-10-29 14:14:33 +1100

    Added previous work

---
+++ FishTank/Assets/Scripts/Fish.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FishState
+{
+    Swimming,
+    Facing
+};
+
+public class Fish : MonoBehaviour
+{
+    public float m_speed;
+    public float m_maxSpeed;
+    public float m_rotationSpeed = 1;
+    public float m_avoidanceDistance;
+    public float m_avoidanceStrength;
+    public Vector3 m_target;
+    public FishMaster m_master;
+
+    Vector3 m_velocity;
+    bool m_hasTarget;
+    FishState m_state = FishState.Swimming;
+    CharacterController m_characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_target = m_master.RandomPosition();
+        m_hasTarget = true;
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (m_state)
+        {
+            case FishState.Swimming:
+                Swimming();
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    m_state = FishState.Facing;                    
+                }
+                break;
+
+            case FishState.Facing:
+                Facing();
+                if (Input.GetKeyDown(KeyCode.Space))
+                    m_state = FishState.Swimming;
+                break;
+        }
+    }
+
+    void Swimming()
+    {
+        if (m_hasTarget)
+        {
+            Vector3 desiredVelocity = (m_target - transform.position).normalized * m_maxSpeed;
+
+            Vector3 steering = desiredVelocity - m_velocity;
+            steering = Vector3.ClampMagnitude(steering, m_speed * Time.deltaTime);
+
+            m_velocity = Vector3.ClampMagnitude(m_velocity + steering, m_maxSpeed);
+
+            ObjectAvoidance();
+
+            //transform.position += m_velocity * Time.deltaTime;
+            m_characterController.Move(m_velocity * Time.deltaTime);
+
+            Quaternion targetRotation = Quaternion.LookRotation(m_velocity);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * m_rotationSpeed);
+
+            if ((m_target - transform.position).magnitude < 0.1)
+                m_hasTarget = false;
+        }
+        else
+        {
+            m_target = m_master.RandomPosition();
+            m_hasTarget = true;
+        }
+    }
+
+    void ObjectAvoidance()
+    {
+        Ray[] rays = new Ray[4];
+        float angle = 30;
+        rays[0] = new Ray(transform.position, transform.rotation * new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad)));
+        rays[1] = new Ray(transform.position, transform.rotation * new Vector3(Mathf.Sin(-angle * Mathf.Deg2Rad), 0, Mathf.Cos(-angle * Mathf.Deg2Rad)));
+        rays[2] = new Ray(transform.position, transform.rotation * new Vector3(0, Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad)));
+        rays[3] = new Ray(transform.position, transform.rotation * new Vector3(0, Mathf.Sin(-angle * Mathf.Deg2Rad), Mathf.Cos(-angle * Mathf.Deg2Rad)));
+
+        foreach (Ray ray in rays)
+        {            
+            Debug.DrawRay(ray.origin, ray.direction * m_avoidanceDistance, Color.blue);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, m_avoidanceDistance, 1 << LayerMask.NameToLayer("Fish")))
+            {
+                m_velocity = Vector3.ClampMagnitude(m_velocity + (-ray.direction * (hit.distance / m_avoidanceDistance * Time.deltaTime * m_avoidanceStrength)), m_maxSpeed);
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+            }
+        }
+    }
+
+    void Facing()
+    {
+        m_velocity -= m_velocity * Time.deltaTime;
+        //transform.position += m_velocity * Time.deltaTime;
+        m_characterController.Move(m_velocity * Time.deltaTime);
+
+        Quaternion targetRotation = Quaternion.LookRotation(m_master.m_camera.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * m_rotationSpeed);
+    }
+}
---
+++ FishTank/Assets/Scripts/FishMaster.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FishType
+{
+    public string TypeName { get { return m_typeName; } }
+    public GameObject PreFab { get { return m_prefab; } }
+    public Vector2 SpeedRange { get { return m_speedRange; } }
+    public Vector2Int SpawnRange { get { return m_spawnRange; } }
+
+    [SerializeField]
+    private string m_typeName;
+    [SerializeField]
+    private GameObject m_prefab;
+    [SerializeField]
+    private Vector2 m_speedRange;
+    [SerializeField]
+    private Vector2Int m_spawnRange;
+}
+
+public class FishMaster : MonoBehaviour
+{
+    public FishType[] m_fishTypes;
+    public Vector3 m_extents;
+    public Transform m_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (FishType fish in m_fishTypes)
+        {
+            int count = Random.Range(fish.SpawnRange.x, fish.SpawnRange.y);
+            for (int i = 0; i < count; i++)
+            {
+                GameObject fishObject = Instantiate(fish.PreFab);
+                fishObject.transform.position = RandomPosition();
+
+                Fish fishScript = fishObject.GetComponent<Fish>();
+                if (fishScript)
+                {
+                    fishScript.m_maxSpeed = Random.Range(fish.SpeedRange.x, fish.SpeedRange.y);
+                    fishScript.m_master = this;
+                }
+                else
+                {
+                    Debug.Log("No Fish script on object");
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 RandomPosition()
+    {
+        Vector3 position = m_extents;
+
+        position.x *= Random.Range(-1f, 1f);
+        position.y *= Random.Range(-1f, 1f);
+        position.z *= Random.Range(-1f, 1f);
+
+        return position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, m_extents * 2);
+    }
+}
